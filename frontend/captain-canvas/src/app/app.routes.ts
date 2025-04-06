7f9ce144fdea78da8ba239604160e77872928d69
samirjdev <samirjdev@outlook.com>
2025-04-06 07:02:37 -0400

    more frontend changes and assets added

--- frontend/captain-canvas/src/app/app.routes.ts
+++ frontend/captain-canvas/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { ResultsComponent } from './components/results/results.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingComponent },         // <--- Default route
+  { path: 'results', component: ResultsComponent },
+  // any other routes ...
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
