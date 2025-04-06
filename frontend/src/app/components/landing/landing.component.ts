a7bfb5cbb07c4cbf0008475ead0743f32adbf1a3
Maxim Ryabinov <2contact.mr@gmail.com>
2025-04-06 10:22:12 -0400

    added more pics and stuff

--- frontend/src/app/components/landing/landing.component.ts
+++ frontend/src/app/components/landing/landing.component.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ApiService } from '../../services/api.service'; // Adjusted the path to the correct location
 
 @Component({
@@ -16,28 +17,31 @@ import { ApiService } from '../../services/api.service'; // Adjusted the path to
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
   apiToken: string = ''; // Holds the user's token
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
   constructor(private apiService: ApiService,  private router: Router) {}
 
-  makeApiRequest() {
-    if (!this.apiToken) {
-      alert('Please enter your API token.');
-      return;
-    }
+  makeApiRequest(): void {
+    this.isLoading = true;
+    this.errorMessage = ''; // Clear any previous error message
 
     this.apiService.getWeeklySchedule(this.apiToken).subscribe(
       (response) => {
+        this.isLoading = false;
         this.router.navigate(['/result'], { state: { data: response } });
       },
       (error) => {
+        this.isLoading = false;
         console.error('API Error:', error);
-        alert('Failed to make the API request. Check the console for details.');
+        this.errorMessage = 'Canvas API token may be invalid or expired. Please regenerate token.';
       }
     );
   }
